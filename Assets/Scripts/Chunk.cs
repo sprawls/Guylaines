4bb312864de96b1af9b615ca34a90437c0c265fb
Martin Lavoie <broemartino@gmail.com>
2015-02-27 23:40:45 -0500

    Chunk are generated and destroyed as needed

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -3,22 +3,23 @@ using System.Collections;
 
 public class Chunk : MonoBehaviour {
 
-    private Vector2 bottomLeft;
-    private Vector2 topRight;
+    private Vector2 _bottomLeft;
+    private Vector2 _topRight;
 	// Use this for initialization
 
 
 	void Start () {
         TerrainGenerator tg = FindObjectOfType<TerrainGenerator>();
         Debug.Log("Start Chunk");
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 100; i++)
         {
             GameObject go = Instantiate(tg.availablePrefab[0]) as GameObject;
             
-            float x = Random.Range(bottomLeft.x, topRight.x);
-            float y = Random.Range(bottomLeft.y, topRight.y);
+            float x = Random.Range(_bottomLeft.x, _topRight.x);
+            float y = Random.Range(_bottomLeft.y, _topRight.y);
 
             go.transform.localPosition = new Vector3(x, 0, y);
+            go.transform.parent = transform;
         }
 	}
 	
@@ -32,9 +33,16 @@ public class Chunk : MonoBehaviour {
         GameObject newObject = new GameObject();
         Chunk yourObject = newObject.AddComponent<Chunk>();
 
-        yourObject.bottomLeft = bottomLeft;
-        yourObject.topRight = topRight;
+        yourObject._bottomLeft = bottomLeft;
+        yourObject._topRight = topRight;
 
         return yourObject;
     }
+
+    public float top { get { return _topRight.y; } }
+    public float right { get { return _topRight.x; } }
+    public float bottom { get { return _bottomLeft.y; } }
+    public float left { get { return _bottomLeft.x; } }
+    public Vector2 bottomLeft { get { return _bottomLeft; } }
+    public Vector2 topRight { get { return _topRight; } }
 }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -16,7 +16,7 @@ public class TerrainGenerator : MonoBehaviour
 
     public List<GameObject> availablePrefab;
 
-    private List<ChunkPair>.Enumerator currentChunk;
+    private int index;
 
     private List<ChunkPair> chunks = new List<ChunkPair>();
 
@@ -24,37 +24,209 @@ public class TerrainGenerator : MonoBehaviour
 	void Start () {
         Random.seed = seed;
         player = GameObject.Find("Ship Prefab");
-        Chunk c = Chunk.Create(new Vector2(-sizeChunk / 2, 0), new Vector2(sizeChunk / 2, sizeChunk));
+
+        Chunk c = Chunk.Create(new Vector2(-sizeChunk, 0), new Vector2(0, sizeChunk));
         ChunkPair cp = new ChunkPair();
         cp.First = c;
         chunks.Add(cp);
-        currentChunk = chunks.GetEnumerator();
-        currentChunk.MoveNext();
+
+        c = Chunk.Create(new Vector2(0, 0), new Vector2(sizeChunk, sizeChunk));
+        cp = new ChunkPair();
+        cp.First = c;
+        chunks.Add(cp);
+        index = 1;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
         generateLeftChunkIfNeeded();
         generateRightChunckIfNeeded();
         generateFrontChunckIfNeeded();
-
+        findCurrent();
         switchLayer();
 	}
 
+    //Update index so that it point to the chunk which contain the player
+    void findCurrent()
+    {
+        Vector3 pos3 = player.transform.localPosition;
+        float x = pos3.x;
+
+        while(x < currentChunk.First.left)
+        {
+            index--;
+        }
+
+        while(x > currentChunk.First.right)
+        {
+            index++;
+        }
+    }
+
     void generateLeftChunkIfNeeded()
     {
-        // currentChunk = Chunk.Create(new Vector2(-250, 0), 500);
+        ChunkPair c = currentChunk;
+        ChunkPair l = leftChunk;
+        bool updated = false;
+        bool shouldUpdate = c.First.left > player.transform.localPosition.x - loadDistance;
+
+        if (l.First == null && shouldUpdate)
+        {
+            Vector2 move = new Vector2(-sizeChunk, 0);
+            Vector2 bl = c.First.bottomLeft;
+            Vector2 tr = c.First.topRight;
+
+            bl += move;
+            tr += move;
+
+            l.First = Chunk.Create(bl, tr);
+            updated = true;
+        }
+
+        if(l.Second == null && shouldUpdate && !updated)
+        {
+            Vector2 move = new Vector2(0, sizeChunk);
+
+            Vector2 bl = l.First.bottomLeft + move;
+            Vector2 tr = l.First.topRight + move;
+
+            l.Second = Chunk.Create(bl, tr);
+            updated = true;
+        }
+
+        if(updated)
+        {
+            leftChunk = l;
+        }
     }
     void generateRightChunckIfNeeded()
     {
+        ChunkPair c = currentChunk;
+        ChunkPair r = rightChunk;
+        bool shouldUpdate = c.First.right < player.transform.localPosition.x + loadDistance;
+        bool updated = false;
 
+        if (r.First == null && shouldUpdate)
+        {
+            Vector2 move = new Vector2(sizeChunk, 0);
+            Vector2 bl = c.First.bottomLeft;
+            Vector2 tr = c.First.topRight;
+
+            bl += move;
+            tr += move;
+
+            r.First = Chunk.Create(bl, tr);
+            updated = true;
+        }
+
+        if (r.Second == null && shouldUpdate && !updated)
+        {
+            Vector2 move = new Vector2(0, sizeChunk);
+
+            Vector2 bl = r.First.bottomLeft + move;
+            Vector2 tr = r.First.topRight + move;
+
+            r.Second = Chunk.Create(bl, tr);
+            updated = true;
+        }
+
+        if (updated)
+        {
+            rightChunk = r;
+        }
     }
     void generateFrontChunckIfNeeded()
     {
-
+        ChunkPair c = currentChunk;
+        Chunk f = c.First;
+        if (c.Second == null && f.top < player.transform.localPosition.z + loadDistance)
+        {
+            c.Second = Chunk.Create(new Vector2(f.left, f.bottom + +sizeChunk), new Vector2(f.right, f.top + sizeChunk));
+            currentChunk = c;
+        }
     }
     void switchLayer()
     {
+        if(player.transform.localPosition.z > currentChunk.First.top)
+        {
+            chunks = chunks.ConvertAll(delegate(ChunkPair p)
+            {
+                if(p.First != null)
+                {
+                    Destroy(p.First.gameObject);
+                }
+                p.First = p.Second;
+                p.Second = null;
+                return p;
+            });
+        }
+    }
 
+    public ChunkPair currentChunk
+    {
+        get
+        {
+            return chunks[index];
+        }
+        set
+        {
+            chunks[index] = value;
+        }
+    }
+
+    public ChunkPair rightChunk
+    {
+        get
+        {
+            if (index + 1 < chunks.Count)
+            {
+                return chunks[index + 1];
+            }
+            else
+            {
+                return new ChunkPair();
+            }
+        }
+
+        set
+        {
+            if (index + 1 < chunks.Count)
+            {
+                chunks[index + 1] = value;
+            }
+            else
+            {
+                chunks.Add(value);
+            }
+        }
+    }
+
+    public ChunkPair leftChunk
+    {
+        get
+        {
+            if(index > 0)
+            {
+                return chunks[index - 1];
+            }
+            else
+            {
+                return new ChunkPair();
+            }
+        }
+
+        set
+        {
+            if (index > 0)
+            {
+                chunks[index - 1] = value;
+            }
+            else
+            {
+                chunks.Insert(0, value);
+                index++;
+            }
+        }
     }
 }
