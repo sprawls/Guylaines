0a94c575133f278b65430a230d7b1c6d8a736218
Alex <aDesjardinsAlex@gmail.com>
2015-02-28 17:45:35 -0500

    item loot

---
+++ Assets/Scripts/LootOnCollision.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootOnCollision : MonoBehaviour {
+
+
+    private ShipControl shipControl;
+    private GameObject playerObj;
+    private int itemStr;
+    // Use this for initialization
+    void Awake()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        shipControl = playerObj.GetComponentInChildren<ShipControl>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            itemStr = collision.gameObject.GetComponentInParent<Item>().basePower;
+            Destroy(collision.gameObject.transform.parent.gameObject);
+            Debug.Log(itemStr);
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
