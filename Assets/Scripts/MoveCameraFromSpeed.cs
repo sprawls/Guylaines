126b6cf8fcf95e9ae6f75e72ce9a5f6bfa397b81
Alex <alex3521@hotmail.fr>
2015-02-27 21:57:36 -0500

    Added Basic Controls

---
+++ Assets/Scripts/MoveCameraFromSpeed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCameraFromSpeed : MonoBehaviour {
+
+	public ShipControl shipControl;
+	public float startDistance;
+	public float endDistance;
+	public float speedForMax = 5f;
+	
+	private Vector3 cameraAngle;
+	private float curSpeed;
+
+	// Use this for initialization
+	void Start () {
+		cameraAngle = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		curSpeed = shipControl.forwardSpeed;
+		float distanceMultiplier = Mathf.Min(speedForMax,curSpeed);
+		distanceMultiplier = Mathf.Lerp(startDistance,endDistance,distanceMultiplier/speedForMax);
+		transform.localPosition = cameraAngle*distanceMultiplier;
+	}
+}
--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -3,13 +3,85 @@ using System.Collections;
 
 public class ShipControl : MonoBehaviour {
 
+	////////////////////////// General //////////////////////////
+	public Transform model;
+	public MoveCameraFromSpeed cameraScript;
+	////////////////////////// Forward Speed //////////////////////////
+	public float forwardSpeed;
+
+	private float speedIncrementPerSecond = 0.2f;
+	private float startSpeed = 0.5f;
+	////////////////////////// Side Speed //////////////////////////
+	public float sideSpeed; //lerp from 0 to maxSideSpeed using curSideSpeedMultiplier as "t"
+	public float tiltAngle;
+	public float control; //change in sideSpeed per second( 1 means it takes 1sec to reach max sidespeed; 2 means it takes 0.5sec)
+	public float stabilizingControlratio = 0.5f; //When no key is pressed, ratio of the control the ship uses to stabilize
+	public float sideSpeedLimit = 0.5f;
+
+
+	public float maxTiltAngle = 5;
+	private float curSideSpeedMultiplier = 0; //Variable between 0 and 1 dictating speed
+
+
+
+
+
+
+
+
+
 	// Use this for initialization
 	void Start () {
-	
+		forwardSpeed = startSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Calculate Forward Speed
+		forwardSpeed += (speedIncrementPerSecond * Time.deltaTime);
+
+		//Calculate Side Speed
+		if(Input.GetAxis("Horizontal") != 0) 
+			ChangeSideSpeed(Input.GetAxis("Horizontal"));
+		else
+			StabilizeSideSpeed();
+
+
+		//Move it
+		transform.position += new Vector3(sideSpeed,0,forwardSpeed); //Move the ship
+		TiltShip(); //Tilt the ship
+	}
+
+	void StabilizeSideSpeed() { //Stabilize the ship when no key is pressed
+		int axis;
+		if(sideSpeed > 0) axis = -1;
+		else axis = 1;
+
+		curSideSpeedMultiplier += Mathf.Sign(axis) * (stabilizingControlratio*control*Time.deltaTime);
+		curSideSpeedMultiplier = Mathf.Clamp(curSideSpeedMultiplier,-1,1);
+
+		sideSpeed = Mathf.Lerp(0, sideSpeedLimit, Mathf.Abs (curSideSpeedMultiplier));
+		sideSpeed *= Mathf.Sign(curSideSpeedMultiplier);
+	}
+
+	void ChangeSideSpeed(float axis) { //Change side speed when a key is pressed
+		//Debug.Log ("side Sped");
+		curSideSpeedMultiplier += Mathf.Sign(axis) * (control*Time.deltaTime);
+		curSideSpeedMultiplier = Mathf.Clamp(curSideSpeedMultiplier,-1,1);
+
+		sideSpeed = Mathf.Lerp(0, sideSpeedLimit, Mathf.Abs (curSideSpeedMultiplier));
+		sideSpeed *= Mathf.Sign(curSideSpeedMultiplier);
+
+	}
+
+	void TiltShip() {
+		//Calculate New Tilt
+		float speedMultiplier = Mathf.Abs(sideSpeed)/sideSpeedLimit;
+		float newTilt = Mathf.Lerp (0,maxTiltAngle,speedMultiplier);
+		newTilt *= Mathf.Sign(-sideSpeed);
+
+		//Apply new Tilt
+		model.localRotation = Quaternion.Euler(new Vector3(0,0,newTilt));
+
 	}
 }
