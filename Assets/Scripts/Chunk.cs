c441b212948b2b9aa84d801d8bbf0dde3f8e2bc1
Martin Lavoie <broemartino@gmail.com>
2015-02-27 22:18:02 -0500

    Add basis for terrain generation

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chunk : MonoBehaviour {
+
+    private Vector2 bottomLeft;
+    private Vector2 topRight;
+	// Use this for initialization
+
+
+	void Start () {
+        TerrainGenerator tg = FindObjectOfType<TerrainGenerator>();
+        Debug.Log("Start Chunk");
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject go = Instantiate(tg.availablePrefab[0]) as GameObject;
+            
+            float x = Random.Range(bottomLeft.x, topRight.x);
+            float y = Random.Range(bottomLeft.y, topRight.y);
+
+            go.transform.localPosition = new Vector3(x, 0, y);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static Chunk Create(Vector2 bottomLeft, Vector2 topRight)
+    {
+        GameObject newObject = new GameObject();
+        Chunk yourObject = newObject.AddComponent<Chunk>();
+
+        yourObject.bottomLeft = bottomLeft;
+        yourObject.topRight = topRight;
+
+        return yourObject;
+    }
+}
---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using ChunkPair = Pair<Chunk,Chunk>;
+
+public class TerrainGenerator : MonoBehaviour 
+{
+    //Initialisation variable
+    public int seed;
+
+    public float sizeChunk = 50;
+    public float loadDistance = 20;
+
+    //Reference to the player
+    private GameObject player;
+
+    public List<GameObject> availablePrefab;
+
+    private List<ChunkPair>.Enumerator currentChunk;
+
+    private List<ChunkPair> chunks = new List<ChunkPair>();
+
+	// Use this for initialization
+	void Start () {
+        Random.seed = seed;
+        player = GameObject.Find("Ship Prefab");
+        Chunk c = Chunk.Create(new Vector2(-sizeChunk / 2, 0), new Vector2(sizeChunk / 2, sizeChunk));
+        ChunkPair cp = new ChunkPair();
+        cp.First = c;
+        chunks.Add(cp);
+        currentChunk = chunks.GetEnumerator();
+        currentChunk.MoveNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        generateLeftChunkIfNeeded();
+        generateRightChunckIfNeeded();
+        generateFrontChunckIfNeeded();
+
+        switchLayer();
+	}
+
+    void generateLeftChunkIfNeeded()
+    {
+        // currentChunk = Chunk.Create(new Vector2(-250, 0), 500);
+    }
+    void generateRightChunckIfNeeded()
+    {
+
+    }
+    void generateFrontChunckIfNeeded()
+    {
+
+    }
+    void switchLayer()
+    {
+
+    }
+}
