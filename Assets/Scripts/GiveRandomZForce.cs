0248c9ecc3ab052ab9b2089581cd3ea4ccc3f269
Alex <alex3521@hotmail.fr>
2015-02-28 13:33:40 -0500

    updated Death animation

---
+++ Assets/Scripts/GiveRandomZForce.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiveRandomZForce : MonoBehaviour {
+
+	private ShipControl shipControl;
+	private bool boostGiven = false;
+	private Vector3 objForce;
+
+	//Use this for initialization
+	void Awake() {
+		GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+		shipControl = playerObj.GetComponentInChildren<ShipControl>();
+	}
+
+	void Start () {
+		float randX = Random.Range (-150,150)*shipControl.forwardSpeed;
+		float randY = Random.Range (150,300)*shipControl.forwardSpeed;
+		float randZ = Random.Range (500,1100)*shipControl.forwardSpeed;
+		objForce = new Vector3(randX,randY,randZ);
+		rigidbody.isKinematic = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(boostGiven == false && shipControl.slowMoEnded == true) {
+			boostGiven = true;
+			GiveForce();
+		}
+	}
+
+	void GiveForce() {
+		rigidbody.isKinematic = false;
+		rigidbody.AddForce(objForce);
+	}
+}
