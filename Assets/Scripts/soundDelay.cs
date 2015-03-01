623a7b173e64c063d5213046a08dcc03d61a2b7e
Alex <alex3521@hotmail.fr>
2015-03-01 14:15:42 -0500

    Added Grinding Sounds

--- Assets/Scripts/soundDelay.cs
+++ Assets/Scripts/soundDelay.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class soundDelay : MonoBehaviour {
 
+	public float delay = 0.2f;
+	private AudioSource audioSource;
 	// Use this for initialization
+
 	void Start () {
-	
+		audioSource = (AudioSource) gameObject.GetComponent<AudioSource>();
+		StartCoroutine(delaySound());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	private IEnumerator delaySound(){
+		yield return new WaitForSeconds(delay);
+		audioSource.Play();
 	}
 }
