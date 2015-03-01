64611545a6e2c80267e85118bd1c1ef8ada425e5
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-03-01 12:43:45 -0500

    distance and best run calc

--- Assets/Scripts/UI/ScoreUIBehaviour.cs
+++ Assets/Scripts/UI/ScoreUIBehaviour.cs
@@ -3,18 +3,24 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ScoreUIBehaviour : MonoBehaviour {
-	
+	public static ScoreUIBehaviour Instance { get; private set; }
+
 	private SectionSlider _runsSlider;
 	private Text _nextRunText;
 
 	private Text _distanceText;
 	private Text _bestText;
 
-
-	public ScoreUIBehaviour Instance { get; private set; }
-
 	void Awake() {
 		Instance = this;
+
+		Transform livesWidget = transform.Find ("Screen Edges/LivesWidget");
+		_runsSlider = livesWidget.Find ("SectionSlider").GetComponent<SectionSlider>();
+		_nextRunText = livesWidget.Find ("ToNext").GetComponent<Text> ();
+
+		Transform distanceWidget = transform.Find ("Screen Edges/DistanceWidget");
+		_distanceText = distanceWidget.Find ("Distance").GetComponent<Text> ();
+		_bestText = distanceWidget.Find ("Best").GetComponent<Text> ();
 	}
 
 	public float RunsRemaining {
