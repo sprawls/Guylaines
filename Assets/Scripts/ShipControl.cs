766075b3fd4bd04c7f56fa3337ee32c54b3b9ee4
Alex <aDesjardinsAlex@gmail.com>
2015-03-01 04:51:51 -0500

    sound fx files

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -43,12 +43,10 @@ public class ShipControl : MonoBehaviour {
 	}
 	void Start () {
 		forwardSpeed = startSpeed;
-        Input.gyro.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(Input.gyro.attitude);
 		//Calculate Forward Speed
         forwardSpeed = startSpeed + (speedIncrementPerLevel* StatManager.Instance.Speed.Level);
 
